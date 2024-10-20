0e04ac507e2398ee662900fb100f81839bd47de5
Giang Bắc <hoangthanhgiang22@gmail.com>
2024-10-20 23:04:01 +0700

    validate nhân viên

--- AppAPI/Controllers/NhanvienController.cs
+++ AppAPI/Controllers/NhanvienController.cs
@@ -30,7 +30,15 @@ namespace AppAPI.Controllers
         public IActionResult Get(int id)
         {
             try
-            {
+			{
+				if (id == null)
+				{
+					return BadRequest("Vui lòng nhập ID!");
+				}
+				if (id == 0)
+				{
+					return BadRequest("Vui lòng nhập ID lớn hơn 0!");
+				}
                 var a = _repos.GetById(id);
                 if(a == null)
                 {
@@ -47,13 +55,57 @@ namespace AppAPI.Controllers
         public IActionResult Put(int id,Nhanvien nv)
         {
             try
-            {
-                var a = _repos.Update(id,nv);
+			{
+				if (id == null)
+				{
+					return BadRequest("Vui lòng nhập ID!");
+				}
+				if (id == 0)
+				{
+					return BadRequest("Vui lòng nhập ID lớn hơn 0!");
+				}
+				if (nv.Hoten==null)
+				{
+					return BadRequest("Vui lòng nhập họ tên!");
+				}
+				if (nv.Hoten.Length>100)
+				{
+					return BadRequest("Vui lòng nhập họ tên không quá 100 ký tự!");
+				}
+				if (nv.Gioitinh == null)
+				{
+					return BadRequest("Vui lòng chọn giới tính!");
+				}
+				if (nv.Sdt == null)
+				{
+					return BadRequest("Vui lòng nhập số điện thoại!");
+				}
+				if (nv.Sdt.Length != 10)
+				{
+					return BadRequest("Vui lòng nhập số điện thoại phải là  10 số!");
+				}
+				if (nv.Trangthai == null)
+				{
+					return BadRequest("Vui lòng chọn trạng thái!");
+				}
+				if (nv.Password == null)
+				{
+					return BadRequest("Vui lòng nhập mật khẩu!");
+				}
+				if (nv.Password.Length > 50)
+				{
+					return BadRequest("Vui lòng nhập mật khẩu không quá 50 ký tự!");
+				}
+				var a = _repos.Update(id,nv);
                 if (a)
                 {
                     return Ok();
-                }
-                return BadRequest();
+				}
+				if (nv.Role == null)
+				{
+					return BadRequest("Vui lòng chọn quyền hạn!");
+				}
+				return BadRequest();
             }
             catch (Exception ex)
             {
@@ -64,8 +116,16 @@ namespace AppAPI.Controllers
         public IActionResult Delete(int id)
         {
             try
-            {
-                var a = _repos.Delete(id);
+			{
+				if (id == null)
+				{
+					return BadRequest("Vui lòng nhập ID!");
+				}
+				if (id == 0)
+				{
+					return BadRequest("Vui lòng nhập ID lớn hơn 0!");
+				}
+				var a = _repos.Delete(id);
                 if (a)
                 {
                     return Ok();
@@ -81,8 +141,40 @@ namespace AppAPI.Controllers
         public IActionResult Post(Nhanvien nv)
         {
             try
-            {
-                var a = _repos.Add(nv);
+			{
+				if (nv.Hoten == null)
+				{
+					return BadRequest("Vui lòng nhập họ tên!");
+				}
+				if (nv.Hoten.Length > 100)
+				{
+					return BadRequest("Vui lòng nhập họ tên không quá 100 ký tự!");
+				}
+				if (nv.Gioitinh == null)
+				{
+					return BadRequest("Vui lòng chọn giới tính!");
+				}
+				if (nv.Sdt == null)
+				{
+					return BadRequest("Vui lòng nhập số điện thoại!");
+				}
+				if (nv.Sdt.Length != 10)
+				{
+					return BadRequest("Vui lòng nhập số điện thoại phải là  10 số!");
+				}
+				if (nv.Trangthai == null)
+				{
+					return BadRequest("Vui lòng chọn trạng thái!");
+				}
+				if (nv.Password == null)
+				{
+					return BadRequest("Vui lòng nhập mật khẩu!");
+				}
+				if (nv.Password.Length > 50)
+				{
+					return BadRequest("Vui lòng nhập mật khẩu không quá 50 ký tự!");
+				}
+				var a = _repos.Add(nv);
                 if (a)
                 {
                     return Ok();
