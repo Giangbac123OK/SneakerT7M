101556ff03c9ad3c37896b97c7ec44f38c35534e
Giang Bắc <hoangthanhgiang22@gmail.com>
2024-10-21 21:21:33 +0700

    Hưng update

---
+++ AppView/IServices/IThuoctinhService.cs
@@ -0,0 +1,13 @@
+﻿using AppData.Models;
+
+namespace AppView.IServices
+{
+    public interface IThuoctinhService
+    {
+        Task<IEnumerable<Thuoctinh>?> GetAllThuocTinhs();
+        Task<Thuoctinh?> GetThuocTinhById(Guid id);
+        Task Create(Thuoctinh thuoctinh);
+        Task Update(Thuoctinh thuoctinh);
+        Task Delete(Thuoctinh id);
+    }
+}
