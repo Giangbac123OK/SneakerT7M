81f0c410a00c5854dce49cb66f9a4762cb3ead09
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2024-10-31 16:02:46 +0700

    ok

---
+++ AppData/Repository/GiohangchitietRepos.cs
@@ -0,0 +1,55 @@
+﻿using AppData.IRepository;
+using AppData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Repository
+{
+    public class GiohangchitietRepos : IGiohangchitietRepos
+    {
+        private readonly MyDbContext _context;
+        public GiohangchitietRepos(MyDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(Giohangchitiet ct)
+        {
+            await _context.giohangchitiets.AddAsync(ct);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var a = await GetByIdAsync(id);
+            if (a != null)
+            {
+                _context.giohangchitiets.Remove(a);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new KeyNotFoundException("Không tìm thấy nhân viên");
+            }
+        }
+
+        public async Task<IEnumerable<Giohangchitiet>> GetAllAsync()
+        {
+            return await _context.giohangchitiets.ToListAsync();
+        }
+
+        public async Task<Giohangchitiet> GetByIdAsync(int id)
+        {
+            return await _context.giohangchitiets.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Giohangchitiet ct)
+        {
+            _context.giohangchitiets.Update(ct);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
