8f60a858c91331e842bce2daed396779fd3b60e5
thanhdz2003 <115550700+thanhdz2003@users.noreply.github.com>
2024-10-31 20:36:17 +0700

    DiaChiAPI

---
+++ AppAPI/Controllers/DiachisController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AppData;
+using AppData.Models;
+using AppData.IService;
+using AppData.Dto;
+using Humanizer;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiachisController : ControllerBase
+    {
+        private readonly IDiaChiService _diaChiService;
+
+        public DiachisController(IDiaChiService diaChiService)
+        {
+            _diaChiService = diaChiService;
+        }
+
+        // GET: api/Diachis
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DiaChiDTO>>> Getdiachis()
+        {
+            try
+            {
+                var item = await _diaChiService.GetAllDiaChi();
+                return Ok(item);
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+
+        // GET: api/Diachis/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DiaChiDTO>> GetDiachi(int id)
+        {
+            try
+            {
+                var diachi = await _diaChiService.GetDiaChiById(id);
+
+                if (diachi == null)
+                {
+                    return NotFound();
+                }
+
+                return diachi;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT: api/Diachis/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDiachi(int id, DiaChiDTO diachi)
+        {
+           
+            try
+            {
+                await _diaChiService.Update(id, diachi);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+        }
+
+        // POST: api/Diachis
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DiaChiDTO>> PostDiachi(DiaChiDTO diachi)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _diaChiService.Create(diachi);
+            return CreatedAtAction(nameof(Getdiachis), new { id = diachi.Diachicuthe }, diachi);
+
+        }
+
+        // DELETE: api/Diachis/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDiachi(int id)
+        {
+            try
+            {
+                 await _diaChiService.Delete(id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ AppData/Dto/DiaChiDTO.cs
@@ -0,0 +1,20 @@
+﻿using AppData.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Dto
+{
+    public class DiaChiDTO
+    {
+
+        public int Idkh { get; set; }
+        public string Thanhpho { get; set; }
+        public string Quanhuyen { get; set; }
+        public string Phuongxa { get; set; }
+        public string? Diachicuthe { get; set; }
+    }
+}
