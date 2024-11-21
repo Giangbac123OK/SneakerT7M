a0688ec8950cfa3b482431438bbc210730f682f9
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2024-11-21 21:03:07 +0700

    Revert "ok"
    This reverts commit 1e1dbb0e118c64868244661dc9b417c183f5dcab.

---
+++ AppData/Dto/ThuoctinhDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Dto
+{
+	public class ThuoctinhDto
+	{
+		[Required(ErrorMessage = "Tên thuộc tính không được để trống")]
+		[MaxLength(100, ErrorMessage = "Tên thuộc tính không được vượt quá 100 ký tự")]
+		public string Tenthuoctinh { get; set; }
+	}
+}
