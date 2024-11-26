b741a293954161c863fe94232f97aef77d518bfd
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2024-11-26 20:50:05 +0700

    ok

--- AppAPI/Program.cs
+++ AppAPI/Program.cs
@@ -29,45 +29,46 @@ builder.Services.AddCors(options =>
 			   .AllowAnyHeader();
 	});
 });
-
-
-
-// ??ng ký các d?ch v? c?a b?n
-builder.Services.AddScoped<IphuongthucthanhtoanRepos, PhuongthucthanhtoanRepos>();
-builder.Services.AddScoped<IphuongthucthanhtoanServicee, PhuongthucthanhtoanService>();
-builder.Services.AddScoped<IGiamgiaRepos, GiamgiaRepos>();
-builder.Services.AddScoped<IGiamgiaService, GiamgiaService>();
-builder.Services.AddScoped<INhanvienRepos, NhanvienRepos>();
-builder.Services.AddScoped<INhanvienService, NhanvienService>();
-builder.Services.AddScoped<InhacungcapRepos, NhacungcapRepos>();
-builder.Services.AddScoped<InhacungcapService, NhacungcapService>();
-
-builder.Services.AddScoped<IsaleRepos, SaleRepos>();
-builder.Services.AddScoped<ISaleService, SaleService>();
-builder.Services.AddScoped<IsalechitietRepos, SaleechitietRepos>();
-builder.Services.AddScoped<ISalechitietService, SalechitietService>();
-builder.Services.AddScoped<IsanphamRepos, SanphamRepos>();
-builder.Services.AddScoped<ISanPhamservice, SanphamService>();
-builder.Services.AddScoped<IThuongHieuRepos, ThuongHieuRepos>();
-builder.Services.AddScoped<IThuongHieuService, ThuongHieuService>();
-builder.Services.AddScoped<IThuocTinhRepos, ThuocTinhRepos>();
-
-builder.Services.AddScoped<IThuoctinhService, ThuocTinhService>();
-builder.Services.AddScoped<ISanphamchitietRepos, SanphamchitietRepos>();
-builder.Services.AddScoped<ISanphamchitietService, SanphamchitietService>();
-builder.Services.AddScoped<ITraHangRepos, TraHangRepos>();
-builder.Services.AddScoped<ITraHangService, TraHangService>();
-builder.Services.AddScoped<ITraHangChiTietRepos, TraHangChiTietRepos>();
-builder.Services.AddScoped<ITraHangChiTietService, TraHangChiTietService>();
-
-builder.Services.AddScoped<IHoadonRepository, HoadonRepos>();
-builder.Services.AddScoped<IHoadonService, HoadonService>();
 builder.Services.AddScoped<IDanhGiaRepos, DanhGiaRepos>();
 builder.Services.AddScoped<IDanhGiaServices, DanhGiaServices>();
-builder.Services.AddScoped<IKhachhangRepos, KhachhangRepos>();
-builder.Services.AddScoped<IKhachhangService, KhachhangService>();
+builder.Services.AddScoped<IDiaChiRepos, DiaChiRepos>();
+builder.Services.AddScoped<IDiaChiService, DiaChiService>();
+builder.Services.AddScoped<IGiamgiaRepos, GiamgiaRepos>();
+builder.Services.AddScoped<IGiamgiaService, GiamgiaService>();
+builder.Services.AddScoped<IGiohangchitietRepos, GiohangchitietRepos>();
+builder.Services.AddScoped<IGiohangchitietService, GiohangchitietService>();
+builder.Services.AddScoped<IGiohangRepos, GiohangRepos>();
+builder.Services.AddScoped<IGiohangService, GiohangService>();
 builder.Services.AddScoped<IHoaDonChiTietRepository, HoaDonChiTietRepos>();
 builder.Services.AddScoped<IHoaDonChiTietService, HoaDonChiTietService>();
+builder.Services.AddScoped<IHoadonRepository, HoadonRepos>();
+builder.Services.AddScoped<IHoadonService, HoadonService>();
+builder.Services.AddScoped<IKhachhangRepos, KhachhangRepos>();
+builder.Services.AddScoped<IKhachhangService, KhachhangService>();
+builder.Services.AddScoped<InhacungcapRepos, NhacungcapRepos>();
+builder.Services.AddScoped<InhacungcapService, NhacungcapService>();
+builder.Services.AddScoped<INhanvienRepos, NhanvienRepos>();
+builder.Services.AddScoped<INhanvienService, NhanvienService>();
+builder.Services.AddScoped<IphuongthucthanhtoanRepos, PhuongthucthanhtoanRepos>();
+builder.Services.AddScoped<IphuongthucthanhtoanServicee, PhuongthucthanhtoanService>();
+builder.Services.AddScoped<IRankRepos, RankRepos>();
+builder.Services.AddScoped<IRankServiece, RankSevi>();
+builder.Services.AddScoped<IsalechitietRepos, SaleechitietRepos>();
+builder.Services.AddScoped<ISalechitietService, SalechitietService>();
+builder.Services.AddScoped<IsaleRepos, SaleRepos>();
+builder.Services.AddScoped<ISaleService, SaleService>();
+builder.Services.AddScoped<ISanphamchitietRepos, SanphamchitietRepos>();
+builder.Services.AddScoped<ISanphamchitietService, SanphamchitietService>();
+builder.Services.AddScoped<IsanphamRepos, SanphamRepos>();
+builder.Services.AddScoped<ISanPhamservice, SanphamService>();
+builder.Services.AddScoped<IThuocTinhRepos, ThuocTinhRepos>();
+builder.Services.AddScoped<IThuoctinhService, ThuocTinhService>();
+builder.Services.AddScoped<IThuongHieuRepos, ThuongHieuRepos>();
+builder.Services.AddScoped<IThuongHieuService, ThuongHieuService>();
+builder.Services.AddScoped<ITraHangChiTietRepos, TraHangChiTietRepos>();
+builder.Services.AddScoped<ITraHangChiTietService, TraHangChiTietService>();
+builder.Services.AddScoped<ITraHangRepos, TraHangRepos>();
+builder.Services.AddScoped<ITraHangService, TraHangService>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
