7604c85d676194c59fe24d97df8667adae73bca6
HoàngThanh Giang <hoangthanhgiang22@gmail.com>
2024-12-11 17:24:14 +0700

    ok

--- AppData/MyDbContext.cs
+++ AppData/MyDbContext.cs
@@ -45,7 +45,7 @@ namespace AppData
 		public DbSet<Trahangchitiet> trahangchitiets { get; set; }
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			optionsBuilder.UseSqlServer("Data Source=HOANGTHANHGIANG\\SQLEXPRESS;Initial Catalog=SneakerT7M;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;Timeout=15082004");
+			optionsBuilder.UseSqlServer("Data Source=HOANGTHANHGIANG\\SQLEXPRESS;Initial Catalog=SneakerT7M;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
 		}
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
