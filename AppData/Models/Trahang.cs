2493c8a63d5d965f56edc609531125668cd2b4f8
Nguyễn Nhật Hưng <145339576+meou2k4@users.noreply.github.com>
2024-12-13 15:38:06 +0700

    update

--- AppData/Models/Trahang.cs
+++ AppData/Models/Trahang.cs
@@ -5,10 +5,24 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace AppData.Models
 {
-	public class Trahang
+    public enum TraHangStatus
+    {
+
+
+        [Description("Đơn hàng chờ trả hàng")]
+        DonhangChoTraHang = 0,
+
+        [Description("Trả hàng thành công")]
+        TraHangThanhCong = 1,
+
+        [Description("Trả hàng không thành công")]
+        TraHangKhongThanhCong = 1,
+    }
+    public class Trahang
 	{
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -30,5 +44,16 @@ namespace AppData.Models
 		public virtual ICollection<Hinhanh> Hinhanhs { get; set; }
 		public virtual ICollection<Trahangchitiet> Trahangchitiets { get; set; }
 
-	}
+        // Trạng thái hiển thị dưới dạng chuỗi
+        public string TrangthaiStr => GetEnumDescription((TraHangStatus)Trangthai);
+
+        // Phương thức để lấy giá trị mô tả từ enum
+        private string GetEnumDescription(TraHangStatus status)
+        {
+            var field = status.GetType().GetField(status.ToString());
+            var attribute = (DescriptionAttribute)Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute));
+            return attribute == null ? status.ToString() : attribute.Description;
+        }
+
+    }
 }
