6829996eb401508375ac2f96044dcd6d8294e68d
Nguyễn Nhật Hưng <145339576+meou2k4@users.noreply.github.com>
2024-12-17 14:31:47 +0700

    sửa lại

--- AppAPI/Controllers/PhuongthucthanhtoanController.cs
+++ AppAPI/Controllers/PhuongthucthanhtoanController.cs
@@ -15,6 +15,7 @@ namespace AppAPI.Controllers
             _service = service;
         }
 
+        //sửa lại
         [HttpGet]
         public async Task<ActionResult<IEnumerable<PhuongthucthanhtoanDTO>>> GetAll()
         {
